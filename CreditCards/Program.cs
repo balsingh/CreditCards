fe04c8235e21f9766befb81a31c5778013000a69
Balbir Singh <balsingh@hotmail.co.uk>
2018-06-19 21:26:07 +0100

    Commit

---
+++ CreditCards/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CreditCards
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                //.UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .UseUrls("http://*:5000")
+                .Build();
+    }
+
+   
+   
+}
